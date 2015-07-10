575e1d64df89f6269420685067c8000967bb654e
huntermaxfield <huntermaxfield1@gmail.com>
2015-07-09 20:02:59 -0600

    done

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,15 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+String.prototype.rev = function() {
+    var newStr = ''
+    for (var i = this.length - 1; i >= 0; i--) {
+        newStr += this[i];
+    }
+    console.log(newStr);
+    return newStr;
+}
+
+var testStr = 'just like you can add';
+testStr.rev();
